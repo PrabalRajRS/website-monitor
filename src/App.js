86cce1eb77333ac3edcaa83e793e9e357a09c852
PRABAL RAJ <96192793+PrabalRajRS@users.noreply.github.com>
2023-04-03 16:34:24 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import WebsiteMonitor from "./websiteMonitor";
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <WebsiteMonitor />
+    </div>
+  );
+}
+
+export default App;
+
+
+
+
+
---
+++ src/websiteMonitor.js
@@ -0,0 +1,69 @@
+import React, { useState, useEffect } from "react";
+
+function App() {
+    const [websiteInput, setWebsiteInput] = useState("");
+    const [websiteList, setWebsiteList] = useState([]);
+
+    const addWebsite = () => {
+        setWebsiteList((prevList) => [
+            ...prevList,
+            { url: websiteInput, status: "Checking..." },
+        ]);
+        setWebsiteInput("");
+    };
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setWebsiteList((prevList) => {
+                const updatedList = prevList.map((website) => {
+                    const url = website.url;
+                    fetch(url)
+                        .then((response) => {
+                            console.log(response)
+                            if (response.ok) {
+                                website.status = "Success";
+                            } else {
+                                website.status = "Failure";
+                            }
+                        })
+                        .catch(() => {
+                            website.status = "Failure";
+                        });
+                    return website;
+                });
+                return updatedList;
+            });
+        }, 120000);
+        return () => clearInterval(interval);
+    }, []);
+
+    return (
+        <div className="website-monitor-container">
+            <input
+                type="text"
+                className="input"
+                value={websiteInput}
+                onChange={(e) => setWebsiteInput(e.target.value)}
+            />
+            <button onClick={addWebsite}>Add</button>
+            <table style={{ borderCollapse: "collapse" }}>
+                <thead>
+                    <tr>
+                        <th>Website</th>
+                        <th>Status</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {websiteList.map((website, index) => (
+                        <tr key={index}>
+                            <td>{website.url}</td>
+                            <td>{website.status}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    );
+}
+
+export default App;
